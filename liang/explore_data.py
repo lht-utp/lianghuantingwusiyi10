2ed490b8f41ae0b3460e7f4e2c25dd1ab307a310
lht-utp <3380846323@qq.com>
2025-10-29 17:12:47 +0800

    Add files via upload

---
+++ liang/explore_data.py
@@ -0,0 +1,10 @@
+#第八章/explore_data.py
+import pandas as pd
+
+pd.set_option('display.unicode.east_asian_width', True)
+
+penguin_df = pd.read_csv('penguins-chinese.csv', encoding='gbk')
+
+print(penguin_df.head())
+
+
